2231449e475197953208f2bd2dea08ca3d74f1e8
Ludovic HENRY <ludovic.henry@me.com>
2020-08-28 10:59:42 -0400

    Initial Commit

---
+++ TestCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCoreData
+//
+//  Created by Ludovic HENRY on 28/08/2020.
+//  Copyright © 2020 Ludovic HENRY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
